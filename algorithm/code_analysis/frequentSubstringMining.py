96408a2ce2024df2e7b4a0b6a95d7dbc359378b6
Wang Yu <wangyu@nfs.iscas.ac.cn>
2016-04-11 14:22:06 +0800

    extract function semantic

--- algorithm/code_analysis/frequentSubstringMining.py
+++ algorithm/code_analysis/frequentSubstringMining.py
@@ -1,6 +1,10 @@
 #coding=utf-8
 import sys
+sys.path.append("..")
 import MySQLdb
+import nlp.semantics_extraction
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 hadoop_output = "/home/wangyu/hadoop/output/part-r-00000"
 output_path = "/home/wangyu/code-mine/result/frequent_word_mining"
@@ -83,9 +87,10 @@ def writeBasicWordsToDB():
         print e
         raise
 
-def insertFunctionCommitMessageViaFiles():
-    func_file_dic = dict()
-    file_func_dic = dict()
+func_file_dic = dict()
+file_func_dic = dict()
+
+def buildFuncFileDic():
     try:
         conn=MySQLdb.connect(host='192.168.162.122',user='wangyu',passwd='123456',port=3306)
         cur=conn.cursor()
@@ -107,9 +112,10 @@ def insertFunctionCommitMessageViaFiles():
     except MySQLdb.Error,e:
         print e
         raise
-    print "finish function file dictionary building..."
+    print "finish function-file dictionary building..."
 
-    file_message_dic = dict()
+file_message_dic = dict()
+def buildFileMessageDic():
     try:
         conn=MySQLdb.connect(host='localhost',user='root',passwd='wangyu',port=3306)
         cur=conn.cursor()
@@ -128,36 +134,64 @@ def insertFunctionCommitMessageViaFiles():
                     file_message_dic[file] += " " + filter_message
                 else:
                     file_message_dic[file] = filter_message
-        print "finish file message dictionary building..."
+        print "finish file-message dictionary building..."
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+        print e
+        raise
 
-        func_message_dic = dict()
+func_message_dic = dict()
+def buildFucMessageDic():
+    try:
         for function in func_file_dic:
             files = func_file_dic[function]
             for file in files:
                 for message_file in file_message_dic:
                     filtered_file = message_file.replace(".../", "")
                     if file.find(filtered_file) != -1:
-                        print file
-                        print filtered_file
                         if function in func_message_dic:
                             func_message_dic[function] += " " + file_message_dic[message_file]
                         else:
                             func_message_dic[function] = file_message_dic[message_file]
         print len(func_message_dic)
-        cur.close()
-        conn.close()
+        print "finish function-message dictionary building..."
     except MySQLdb.Error,e:
         print e
         raise
+
+n_samples = 2000
+n_features = 1000
+n_topics = 1
+n_top_words = 10
+def extractTopicLDA():
+    output = open("../../result/topic_extraction", "wr")
     try:
         conn=MySQLdb.connect(host='192.168.162.122',user='wangyu',passwd='123456',port=3306)
         cur=conn.cursor()
         cur.execute('set names utf8mb4')
         conn.select_db('codeAnalysis')
-        for function, message in func_message_dic:
-            sql = "update func_semantic set commit_messages = '"+message+"' where func_name = '"+function+"'"
+        for function in func_message_dic:
+            message = func_message_dic[function]
+            np_extractor = nlp.semantics_extraction.NPExtractor(message)
+            text = np_extractor.extract()
+            if len(text) == 0:
+                continue
+            tf_vectorizer = CountVectorizer(max_df=1.0, min_df=1, max_features=n_features, stop_words='english')
+            tf = tf_vectorizer.fit_transform(text)
+            print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..." % (n_samples, n_features))
+            lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_method='online', learning_offset=50.,
+                                                                    random_state=0)
+            lda.fit(tf)
+            tf_feature_names = tf_vectorizer.get_feature_names()
+            seprator = " "
+            for topic_idx, topic in enumerate(lda.components_):
+                keywords = seprator.join([tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+            sql = "update func_semantic set semantic = '"+keywords+"' where func_name = '"+function+"'"
+            print keywords
             cur.execute(sql)
-        cur.commit()
+            conn.commit()
+        cur.close()
         conn.close()
     except MySQLdb.Error,e:
         print e
@@ -171,13 +205,18 @@ def filterMessage(message):
         word = filter(str.isalnum, str(word))
         if word != "":
             filtered_setence += word + " "
-            filtered_setence = filtered_setence.rstrip()
-            filtered_setence += "."
+    filtered_setence = filtered_setence.rstrip()
+    filtered_setence += "."
     return filtered_setence
+
+def executeSequence():
+    buildFuncFileDic()
+    buildFileMessageDic()
+    buildFucMessageDic()
+    extractTopicLDA()
 
 if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding( "utf-8" )
-    #writeBasicWordsToDB()
-    insertFunctionCommitMessageViaFiles()
+    executeSequence()
     
